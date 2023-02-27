818b44a94cd874be4777fee3c5fbef119de6e21b
Shusil Shapkota <shusilshapkota@gmail.com>
2023-02-27 12:37:46 -0500

    changed the code from github web editor

---
+++ src/main/java/ca/uwaterloo/cs446/designpatterns/adapter/CircleAdapter.java
@@ -0,0 +1,18 @@
+package ca.uwaterloo.cs446.designpatterns.adapter;
+
+public class CircleAdapter implements Shape {
+	private Circle adaptee;
+	
+	public CircleAdapter(Circle circle) {
+		this.adaptee = circle;
+	}
+	
+	@Override
+	public void draw(int x1, int y1, int x2, int y2) {
+        int radius = Math.abs(x2-x1);
+        int x = radius + x1
+        int y = radius + y1
+		
+		adaptee.draw(x, y, radius);
+	}
+}
