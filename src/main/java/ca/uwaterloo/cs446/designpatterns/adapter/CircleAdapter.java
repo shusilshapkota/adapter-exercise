5d3d62518554d4ec05a046da09f9651237c31c9d
Shusil Shapkota <shusilshapkota@gmail.com>
2023-02-27 12:39:34 -0500

    fixed compilation error

--- src/main/java/ca/uwaterloo/cs446/designpatterns/adapter/CircleAdapter.java
+++ src/main/java/ca/uwaterloo/cs446/designpatterns/adapter/CircleAdapter.java
@@ -10,8 +10,8 @@ public class CircleAdapter implements Shape {
 	@Override
 	public void draw(int x1, int y1, int x2, int y2) {
         int radius = Math.abs(x2-x1);
-        int x = radius + x1
-        int y = radius + y1
+        int x = radius + x1;
+        int y = radius + y1;
 		
 		adaptee.draw(x, y, radius);
 	}
